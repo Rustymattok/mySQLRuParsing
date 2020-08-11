968d5420353c6818920f941dbbef3f89f8454113
Rustymattok <mrArxi@gmail.com>
2020-08-11 09:42:42 +0300

    update format date of test

--- src/test/java/ru/makarov/dao/JDBCDataTest.java
+++ src/test/java/ru/makarov/dao/JDBCDataTest.java
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.scripts.JD;
 import org.junit.Test;
 import ru.makarov.model.Post;
 
+import java.util.Date;
 import java.util.List;
 
 public class JDBCDataTest {
@@ -14,18 +15,16 @@ public class JDBCDataTest {
         Post post = new Post();
         post.setName("Java");
         post.setDescription("My Dream is Java1");
-        post.setFormatDate("24 авг 19, 20:23");
+        post.setDate(new Date());
         jdbcData.add(post);
         Post post1 = new Post();
         post1 = jdbcData.selectByTag("My Dream is Java");
-        System.out.println(post1.getDescription());
     }
 
     @Test
     public void whenShouldCheallAllPost(){
         JDBCData jdbcData = new JDBCData();
         List<Post> list = jdbcData.allPost();
-        System.out.println(list.size());
     }
 
 }
